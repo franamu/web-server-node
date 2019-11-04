a8641a40aaa9623cd146c289e25fcea4815bd35c
Francisco <franamuchastegui@gmail.com>
2019-11-04 08:45:30 -0300

    primer commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+var hbs = require('hbs');
+
+app.use( express.static( __dirname + '/public') );
+
+// Express hbs engine
+hbs.registerPartials( __dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+ 
+app.get('/', (req, res) => {
+
+	res.render('home', {
+		nombre: 'Francisco',
+		anio: new Date().getFullYear()
+	});
+})
+ 
+app.listen(3000, () => {
+	console.log('Escuchando peticiones en el puerto 3000');
+})
